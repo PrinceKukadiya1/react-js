4472555af7f33bdd6e241515000cedd371d76695
prince kukadiya <princekukadiya2304@gmail.com>
2025-03-16 14:35:20 +0530

    props understanding

---
+++ props/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-500 bg-amber-300'>App</div>
+    </>
+  )
+}
+
+export default App
